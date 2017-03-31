7d08bf2844b87fe714e9a26f633600a47b92456d
estrattonbailey <ericstrattonbailey@gmail.com>
2017-03-31 10:06:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# tackjs
+
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](http://standardjs.com)
+
+## Features
+
+## Usage
+
+## Browser Support
+TODO, but should work in all evergreen browsers and IE 10+.
+
+## Example
+To run the example, clone this repo, then:
+```bash
+# move into example dir
+cd tackjs/example
+# install deps
+npm i
+# compile JS
+npm run js:build # or js:watch
+# serve index.html and update with changes
+live-server 
+```
+
+MIT License
---
+++ package/index.js
@@ -0,0 +1,63 @@
+export const getCoords = element => {
+  const { left: l, right: r, top: t, bottom: b } = element.getBoundingClientRect()
+  const { pageYOffset: y } = window
+
+  return {
+    top: {
+      y: y + t,
+      x: l + ((r - l) / 2)
+    },
+    bottom: {
+      y: y + b,
+      x: l + ((r - l) / 2)
+    },
+    left: {
+      y: t + ((b - t) / 2),
+      x: l
+    },
+    right: {
+      y: t + ((b - t) / 2),
+      x: r
+    },
+    topLeft: {
+      y: y + t,
+      x: l
+    },
+    bottomLeft: {
+      y: y + b,
+      x: l
+    },
+    topRight: {
+      y: y + t,
+      x: r
+    },
+    bottomRight: {
+      y: y + b,
+      x: r
+    }
+  }
+}
+
+export const position = (target, scope, placement) => {
+  const c = getCoords(scope)[placement]
+  const e = getCoords(target)
+  const x = c.x - ((e.right.x - e.left.x) / 2)
+  const y = c.y - ((e.bottom.y - e.top.y) / 2)
+
+  target.classList.add('is-tacked')
+  target.style.transform = `translateX(${x}px) translateY(${y}px)`
+}
+
+export const tack = (target, scope, placement) => {
+  position(target, scope, placement)
+
+  return {
+    update () {
+      position(target, scope, placement)
+    },
+    destroy () {
+      target.style.transform = ''
+      target.classList.remove('is-tacked')
+    }
+  }
+}
