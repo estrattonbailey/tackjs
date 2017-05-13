8f040ec5ae1d83e6ed8d33d14017384d258c7bc5
estrattonbailey <ericstrattonbailey@gmail.com>
2017-05-13 15:04:37 -0400

    Updload updated demo

---
+++ index.js
@@ -0,0 +1,35 @@
+import { tack } from '../package/dist/index.js'
+
+document.addEventListener('DOMContentLoaded', e => {
+  window.target = document.getElementById('target')
+  window.box = [
+    document.getElementById('box0'),
+    document.getElementById('box1'),
+    document.getElementById('box2'),
+    document.getElementById('box3')
+  ]
+  window.tack = tack
+
+  console.group('tackjs')
+  console.log(`welcome to the tack demo 📌
+
+attached to the window are a few objects for you to mess with:
+  1. target (the small black square you see in the top left)
+  2. box (array of the tomato colored squares)
+  3. tack (contructor function)
+
+use them like this:
+
+  tack(target, box[1], 'top')
+
+also, tack returns some utils:
+
+  const pin = tack(target, box[3], 'bottomLeft')
+
+  pin.update() // update position, like after resize
+  pin.destroy() // unpin
+
+bonus points: attach an event listener, like resize, and call update on each tick to maintain pinned position.
+  `)
+  console.groupEnd()
+})
